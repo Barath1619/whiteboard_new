f952961296463e56c5c1d645cff4240d07f5f08d
Barath1619 <barath1619a@gmail.com>
2022-12-06 14:42:33 +0000

    new change

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
-from flask import Flask 
+from flask import Flask
 application = Flask(__name__)
+
 
 @application.route('/')
 def hellow_world():
     return 'Hi hellow world'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8080, debug=True)
