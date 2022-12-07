56e33af31ffb80e05f3d1d2b446da8f7154df32f
Barath1619 <barath1619a@gmail.com>
2022-12-07 00:25:12 +0000

    new change

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ application = Flask(__name__)
 
 @application.route('/')
 def hellow_world():
-    return 'Hi hellow world'
+    return 'Hi Dhanush'
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8080, debug=True)
+    app.run('0.0.0.0')
